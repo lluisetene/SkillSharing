a22d1960cb4c7f1849e75313bdd4db7c05d0d819
SCH <SCH@SCH>
2017-05-04 19:26:22 +0200

    dfsfsdf

--- SkillSharing/src/es/uji/ei1027/skillsharing/validators/Validator.java
+++ SkillSharing/src/es/uji/ei1027/skillsharing/validators/Validator.java
@@ -6,5 +6,5 @@ public interface Validator {
 	public boolean supports(Class<?> cls);
 	public void validateAdd(Object obj, Errors errors);
 	public void validateSearch(Object obj, Errors errors);
-	// prueba con mi usuario y contraseña
+	// prueba con mi usuario y contraseñaefv
 }
